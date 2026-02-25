0bf7155a025f3357eb57c30b67048c3abe0d0b46
Brhandon Palomo <brhandon28@gmail.com>
2026-02-24 20:56:38 -0400

    add posts app with feed view and URL configuration

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,13 @@
+# # posts/views.py
+# from django.shortcuts import render
+# from django.http import HttpResponse
+
+# def feed(request):
+#     return HttpResponse("¡Feed funcionando!")
+
+# posts/views.py
+
 from django.shortcuts import render
 
-# Create your views here.
+def feed(request):
+    return render(request, 'posts/feed.html')
